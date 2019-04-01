0082e33b2d9771c2348264a2b370d46249aa8c64
Alberto Jimenez <alberto.jimenez@grupocmc.es>
2019-04-01 10:52:28 +0200

    Try embedded redis

--- src/test/java/com/gruposantander/subscribersarq/services/CustodianServiceIT.java
+++ src/test/java/com/gruposantander/subscribersarq/services/CustodianServiceIT.java
@@ -3,6 +3,9 @@ package com.gruposantander.subscribersarq.services;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +17,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.gruposantander.subscribersarq.models.Custodian;
 import com.gruposantander.subscribersarq.repositories.CustodianRepository;
+import redis.embedded.RedisServer;
 
+import java.io.IOException;
+
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -28,9 +35,28 @@ public class CustodianServiceIT {
 	@Autowired
 	private CustodianRepository custodianRepository;
 
+	private final static int PORT_REDIS = 6379;
+
+	private static RedisServer redisServer;
+
+	@BeforeClass
+	public static void before() {
+		try {
+			redisServer = new RedisServer(PORT_REDIS);
+		} catch (IOException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		}
+		redisServer.start();
+	}
+
+	@AfterClass
+	public static void after() {
+		redisServer.stop();
+	}
+
 	@Test
 	public void testSave() {
-
 		Custodian custodianMock = Custodian.builder().hash("0000002").uri("http://ejemplo2.es").proc("P2").version("v2.3.l8")
 				.information("Esto es un comentario").build();
 		Custodian custodian = this.custodianService.save(custodianMock);
